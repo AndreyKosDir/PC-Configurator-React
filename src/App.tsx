86870477e8ec392ecb54e707c066d345e3c95ccc
Dragon <maidebura.andrei@gmail.com>
2023-11-09 23:05:47 +0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import DetailsPanel from "./components/DetailsPanel";
-import DataLoader from "./data/DataLoader";
+import DetailsPanel from './components/DetailsPanel';
+import DataLoader from './data/DataLoader';
 
 function  App({dataLoader}: {dataLoader: DataLoader}) {
   const detailsViewData = dataLoader.getDetailsViewData();
